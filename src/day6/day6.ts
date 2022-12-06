7af25bf28b26d11a1149f63db899fb7bf1cd58bd
waeekron <waeekron@LAPTOP-BJADCT21.localdomain>
2022-12-06 09:14:13 +0200

    refactoring:)

--- src/day6/day6.ts
+++ src/day6/day6.ts
@@ -1,14 +1,13 @@
 import { readFileSync } from "fs"
 
 const data = readFileSync("in6.txt", "utf-8").split("")
-const BUFFER_LEN = 14 // 4 for part 1, 14 for part 2
+const BUFFER_LEN = 4 // 4 for part 1, 14 for part 2
 let amountProcessed = 0
 for (const [i, _] of data.entries()) {
   if (i === data.length - BUFFER_LEN - 1) break
-  const buffer = new Set([...data.slice(i, i + BUFFER_LEN)])
+  const buffer = new Set(data.slice(i, i + BUFFER_LEN))
   if (buffer.size === BUFFER_LEN) {
     amountProcessed = i + BUFFER_LEN
-    console.log(buffer)
     break
   }
 }
