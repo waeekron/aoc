3c0c4e33ce01c8950ebda5eed9c1b7be17a7f6af
waeekron <waeekron@LAPTOP-BJADCT21.localdomain>
2023-12-02 22:03:27 +0200

    day02 parts 1 & 2

--- 2023/02/main.go
+++ 2023/02/main.go
@@ -60,62 +60,50 @@ func main() {
 		games = append(games, sets)
 		sets = make([]Set, 0)
 	}
-	fmt.Println(len(gameIDs))
-	fmt.Println(len(sets))
-	fmt.Println("\n")
-
 	/*
-		only 12 red cubes, 13 green cubes, and 14 blue cubes
-		game is not possible if one draw has more than above amount of certain color cube
+		part 1
+			only 12 red cubes, 13 green cubes, and 14 blue cubes
+			game is not possible if one draw has more than above amount of certain color cube
+		part 2
+			figure out the fewest number of cubes of each color for a game
 	*/
 
 	sum := 0
 	sum2 := 0
-
 	count := 0
+
 	for i, sets := range games {
 		var largestBlue, largestRed, largestGreen int
-		fmt.Println("Game:", sets)
 		possible := false
 		for _, set := range sets {
 			handIsPossible := true
-			//for _, draw := range set.draws {
+			for _, draw := range set.draws {
+				a := draw.amount
+				switch color := draw.color; color {
+				case "red":
+					if a > 12 {
+						handIsPossible = false
+						break
+					}
+				case "blue":
+					if a > 14 {
+						handIsPossible = false
+						break
+					}
+				case "green":
 
-			//	fmt.Println("Iterating over this set:", set)
-			//	a := draw.amount
-			//	switch color := draw.color; color {
-			//	case "red":
-			//		if a > 12 {
-			//			handIsPossible = false
-			//			fmt.Println("BREAKIN FROM THE FIRST LOOP")
-			//			break
-			//		}
-			//	case "blue":
-			//		if a > 14 {
-			//			handIsPossible = false
-			//			break
-			//		}
-			//	case "green":
-
-			//		if a > 13 {
-			//			handIsPossible = false
-			//			break
-			//		}
-			//	default:
-			//		// do nothing
-			//	}
-			//}
-			fmt.Println("????????????????????????????????????")
+					if a > 13 {
+						handIsPossible = false
+						break
+					}
+				default:
+					// do nothing
+				}
+			}
 			for _, draw := range set.draws {
 				a := draw.amount
 				count++
 
-				fmt.Println("Iterating over this set:", set)
-				fmt.Println(draw, largestGreen, largestBlue, largestRed, "MIKÄ VITTU")
-				if i == 2 {
-					fmt.Println("okei meidän pitäisi nyt ajaa kyseisen arvon yli")
-				}
-				fmt.Println(draw.color == "green", draw.color == "red", draw.color == "blue")
 				switch color := draw.color; color {
 				case "red":
 
@@ -142,24 +130,19 @@ func main() {
 				possible = true
 			} else {
 				possible = false
-				break
 			}
-			fmt.Println("\n\n")
 		}
 		product := largestRed * largestBlue * largestGreen
 
-		println(product, "tulo", largestGreen, largestBlue, largestRed)
 		largestGreen, largestRed, largestBlue = 0, 0, 0
 		sum2 += product
 		if possible {
 			sum += i + 1
 			possible = false
 		}
-		fmt.Println("------------------------")
 	}
 	fmt.Printf("part 1: %v\n", sum)
 	fmt.Printf("part 2: %v\n", sum2)
-	fmt.Print("MITÄ HELVETTIÄ")
 
 	fmt.Println("count", count)
 }
