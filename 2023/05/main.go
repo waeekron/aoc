074809f0409084a850fb94bc4516a4668103a8da
waeekron <waeekron@LAPTOP-BJADCT21.localdomain>
2023-12-09 01:19:31 +0200

    takes 27m to run :)))

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -30,23 +31,50 @@ func main() {
 	fmt.Println(maps[0][0].rangeLength)
 	seeds := strings.Split(strings.TrimSpace(strings.Split(parts[0], ":")[1]), " ")
 
-	seedRanges := collectSeedRanges(seeds)
-	for _, sr := range seedRanges {
-		// iterate over all the seeds in seedRange
-
-		for sr.start < sr.end {
-
-			seed := sr.start
-			fmt.Println("seed-end", seed, sr.end)
-			// iterate over all the maps for a seed
-			for range maps {
-				//fmt.Println("fromTo", fromTo)
+	length := getLength(seeds)
+	allSeeds := collectSeeds(seeds, length)
+	_ = allSeeds
+	fmt.Println("Starting to look for smallest location")
+	smallesLocation := math.MaxInt64
+	for i := range allSeeds {
+		cur := allSeeds[i]
+		// iterate through maps
+		for _, m := range maps {
+			b := false
+			for _, r := range m {
+				if r.source.start <= cur && cur <= r.source.end {
+					cur = cur - r.source.start + r.destination.start
+					b = true
+					break
+				}
 			}
-			sr.start++
+			if b {
+				continue
+			}
+		}
+
+		if smallesLocation == math.MaxInt64 || smallesLocation > cur {
+			smallesLocation = cur
 		}
 	}
 
+	//for _, sr := range seedRanges {
+	//	// iterate over all the seeds in seedRange
+
+	//	for sr.start < sr.end {
+
+	//		seed := sr.start
+	//		fmt.Println("seed-end", seed, sr.end)
+	//		// iterate over all the maps for a seed
+	//		for range maps {
+	//			//fmt.Println("fromTo", fromTo)
+	//		}
+	//		sr.start++
+	//	}
+	//}
+
 	fmt.Println("Code took", time.Since(t), "to run")
+	fmt.Println(smallesLocation)
 }
 
 func collectMaps(m []string) [][]Mapping {
@@ -74,18 +102,44 @@ func collectMaps(m []string) [][]Mapping {
 	}
 	return maps
 }
-
-func collectSeedRanges(seeds []string) []Range {
-
-	var ranges []Range
+func collectSeeds(seeds []string, l int) []int {
+	fmt.Println("Generate all the seeds")
+	var allSeeds = make([]int, l)
+	var j int
 	for i := 0; i < len(seeds); i += 2 {
 		s, e := seeds[i], seeds[i+1]
 		start, _ := strconv.Atoi(s)
 		end, _ := strconv.Atoi(e)
 		end += start
-		r := Range{start: start, end: end}
-		ranges = append(ranges, r)
+		for start < end {
+			allSeeds[j] = start
+			start++
+			j++
+		}
 	}
 
-	return ranges
+	return allSeeds
 }
+func getLength(seeds []string) int {
+	var l int
+	for i := 0; i < len(seeds); i += 2 {
+		rl, _ := strconv.Atoi(seeds[i+1])
+		l += rl
+	}
+	return l
+}
+
+//func collectSeedRanges(seeds []string) []Range {
+//
+//	var ranges []Range
+//	for i := 0; i < len(seeds); i += 2 {
+//		s, e := seeds[i], seeds[i+1]
+//		start, _ := strconv.Atoi(s)
+//		end, _ := strconv.Atoi(e)
+//		end += start
+//		r := Range{start: start, end: end}
+//		ranges = append(ranges, r)
+//	}
+//
+//	return ranges
+//}
