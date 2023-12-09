0a35a480a0b745cf7f2eed5695e592d20e653daf
waeekron <waeekron@LAPTOP-BJADCT21.localdomain>
2023-12-10 01:53:17 +0200

    day 6 parts 1 and 2

--- 2023/06/main.go
+++ 2023/06/main.go
@@ -1,7 +1,124 @@
 package main
 
-import "bufio"
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+Time:      7  15   30
+Distance:  9  40  200
+*/
+
+type Boat struct {
+	speed int
+}
+
+func (b *Boat) holdButton(ms int) {
+	b.speed = ms
+}
+
+func (b Boat) travel(ms int) int {
+	return b.speed * ms
+}
 
 func main() {
-	bufio.NewScanner()
+	bytes, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic("No input")
+	}
+	content := string(bytes)
+	// parse input
+	parts := strings.Split(content, "\n")
+	part1(parts)
+	part2(parts)
+}
+func part2(parts []string) {
+	t := strings.Split(strings.TrimSpace(strings.Split(parts[0], ":")[1]), " ")
+	d := strings.Split(strings.TrimSpace(strings.Split(parts[1], ":")[1]), " ")
+	times, err := combine(t)
+	if err != nil {
+		panic(err)
+	}
+
+	distances, err := combine(d)
+	if err != nil {
+		panic(err)
+	}
+	count := 1
+
+	for i := 0; i < len(times); i++ {
+		t := times[i]
+		d := distances[i]
+		waysToWin := 0
+
+		boat := new(Boat)
+		tick := 0
+		for boat.speed < t {
+			boat.holdButton(tick)
+			if boat.travel(t-tick) > d {
+				waysToWin++
+			}
+
+			tick++
+
+		}
+		count *= waysToWin
+	}
+	fmt.Println("Count of ways to win", count)
+}
+
+// [1, 2, 4] -> [124]
+func combine(a []string) ([]int, error) {
+	var n string
+	for _, s := range a {
+		n += s
+	}
+	num, err := strconv.Atoi(n)
+	if err != nil {
+		return []int{0}, err
+	}
+	return []int{num}, nil
+}
+func part1(parts []string) {
+
+	t := strings.Split(strings.TrimSpace(strings.Split(parts[0], ":")[1]), " ")
+	d := strings.Split(strings.TrimSpace(strings.Split(parts[1], ":")[1]), " ")
+	times := convStr(t)
+	distances := convStr(d)
+	count := 1
+
+	for i := 0; i < len(times); i++ {
+		t := times[i]
+		d := distances[i]
+		waysToWin := 0
+
+		boat := new(Boat)
+		tick := 0
+		for boat.speed < t {
+			boat.holdButton(tick)
+			if boat.travel(t-tick) > d {
+				waysToWin++
+			}
+
+			tick++
+
+		}
+		count *= waysToWin
+	}
+	fmt.Println("Count of ways to win", count)
+}
+
+func convStr(s []string) []int {
+	var a []int
+	for i := range s {
+		num, err := strconv.Atoi(s[i])
+		if err != nil {
+			continue
+		}
+		a = append(a, num)
+	}
+	return a
 }
