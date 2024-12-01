e04c8f644932440cd46deff3edd2020af4cc2ae3
waeekron <waeekron@student.jyu.fi>
2024-12-01 19:23:58 +0200

    refactor day1

--- 2024/app/src/main/kotlin/aoc/day01/Day01.kt
+++ 2024/app/src/main/kotlin/aoc/day01/Day01.kt
@@ -3,47 +3,45 @@ package aoc.day01
 import readInput
 import kotlin.math.abs
 
-fun part1(input: List<String>) {
-    val pairs = input.map {
-         it.split("   ").map { s -> s.toInt() }
-    }
-    var left = pairs.map {
-        it[0]
-    }
-    var right = pairs.map {
-        it[1]
-    }
-
+fun part1(){
+    val pairs = parse()
+    var left = pairs.first
+    var right = pairs.second
     left = left.sorted()
     right = right.sorted()
-
     var sumOfDistances = 0
 
-    for (i in 0..pairs.lastIndex ) {
+    for (i in left.indices) {
         sumOfDistances += abs(left[i]- right[i])
     }
+
     println(sumOfDistances)
 }
 
-fun part2(input: List<String>) {
-    val pairs = input.map { it.split("   ").map { s -> s.toInt() } }
-    val left = pairs.map { it[0] }
-    val right = pairs.map { it[1] }
+fun part2() {
+    val pairs = parse()
+    val left = pairs.first
+    val right = pairs.second
     var similarityScore = 0
-    for((idx, value) in left.withIndex()) {
 
-       val count=    right.count{ it == value}
-
-        similarityScore += value*count
-
-
-
+    left.forEach {
+        similarityScore += it + right.count {value -> value == it}
     }
 
     println(similarityScore)
 
 }
+
+fun parse(): Pair<List<Int>, List<Int>> {
+  val pairs = readInput("input01.txt").map {
+      it.split("   ").map { s -> s.toInt() }
+  }
+    val left = pairs.map { it[0]}
+    val right= pairs.map { it[1]}
+    return Pair(left, right)
+}
+
 fun main() {
-    part1(readInput("input01.txt"))
-    part2(readInput("input01.txt"))
+    part1()
+    part2()
 }
