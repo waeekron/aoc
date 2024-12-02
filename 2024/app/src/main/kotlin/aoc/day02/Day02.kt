f64954e23528a0be39e00cbe49a61c1d67fab417
waeekron <waeekron@student.jyu.fi>
2024-12-02 21:00:13 +0200

    day2 part 2

--- 2024/app/src/main/kotlin/aoc/day02/Day02.kt
+++ 2024/app/src/main/kotlin/aoc/day02/Day02.kt
@@ -5,7 +5,8 @@ import kotlin.math.abs
 
 
 fun main() {
-    part1()
+//    part1()
+    part2()
 }
 
 fun parse(): List<List<Int>> =
@@ -46,4 +47,65 @@ fun part1() {
         }
     }
     println(safeCount)
+}
+
+fun part2() {
+    var safeCount = 0
+    val raports = parse()
+    val checkAgain = emptyList<List<Int>>().toMutableList()
+    val safeRaports = emptyList<List<Int>>().toMutableList()
+    for (raport in raports) {
+
+        var safe = true
+        if (checkRaport(raport)) {
+            safeCount++
+            safeRaports.add(raport)
+        } else {
+            checkAgain.add(raport)
+        }
+
+    }
+    for (raport in checkAgain) {
+        for (i in 0..<raport.size) {
+            val candidate = raport.toMutableList().filterIndexed { idx, _ -> idx != i }
+            if (checkRaport(candidate)) {
+
+                safeCount++
+
+                print("candidate ")
+                println(candidate.toString())
+
+                break
+            }
+        }
+    }
+    println(safeCount)
+}
+
+fun checkRaport(raport: List<Int>): Boolean {
+    var safe = true
+    val increasing = raport[0] < raport[1]
+    for (i in 0..<raport.size - 1) {
+        val difference = abs(raport[i] - raport[i + 1])
+        if (difference == 0 || difference < 1 || difference > 3) {
+            safe = false
+            break
+        }
+        if (i > 1 && i < raport.size - 1) {
+            if (increasing) {
+                if (!(raport[i - 1] < raport[i] && raport[i] < raport[i + 1])) {
+                    safe = false
+                    break
+                }
+            } else {
+                if (!(raport[i - 1] > raport[i] && raport[i] > raport[i + 1])) {
+                    safe = false
+                    break
+                }
+            }
+
+        }
+
+    }
+    return safe
 }
